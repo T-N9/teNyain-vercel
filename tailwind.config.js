d58b9202d1eef42489334172d19a6e42a66eb3a9
T-N9 <thethoriumtension@gmail.com>
2021-06-13 14:40:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        tn:{
+          colors:{
+            bgColor:['#F5F5F7'],            
+            darkColor:['#1D1D1F'],
+            dark2Color:['#D5D5D4'],
+            dark3Color:['#86868B'],
+            linkColor:['#0066CC'],
+            redColor:['#CC2F36']
+          }
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
